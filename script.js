2e955b878cbcd03f9c1e44ad473e31d554d7588f
AreebRehman5 <136895724+AreebRehman5@users.noreply.github.com>
2023-11-10 10:55:43 +0500

    Add files via upload
    Signed-off-by: AreebRehman5 <136895724+AreebRehman5@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,141 +1,113 @@
-// ===> Question : 1 <===
-let showDate = document.getElementById("date");
-let now = new Date();
-let month = now.getMonth() + 1;
-let day = now.getDate() + 1;
-let year = now.getFullYear();
-showDate.innerHTML = day + " / " + month + " / " + year;
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js'
 
-// ===> Question : 2 <===
-function dobCheck() {
-  let date = document.getElementById("userInput");
-  let userDob = new Date(date.value);
-  let now = new Date();
-  let difference = now.getFullYear() - userDob.getFullYear();
-  console.log(difference);
-  let countdown = document.getElementById("userAge");
-  countdown.innerHTML = difference;
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTGp9HLPjpAOTNUfsRrcHVkTTYXOWlCrA",
+  authDomain:  "blog-firebase-b6377.firebaseapp.com",
+  databaseURL: 'https://blog-firebase-b6377.appspot.com',
+  projectId: "blog-firebase-b6377",
+  storageBucket:  "blog-firebase-b6377.appspot.com",
+  messagingSenderId: '987255097152',
+  appId: "1:217587276904:web:1543ecc25d06249e06efdd",
 }
 
-// ===> Question : 3 <===
-// let daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-// let userInput = prompt("Enter the Date")
-// let now = new Date(userInput)
-// let day = now.getDay()
-// let days = daysName[day]
-// if (!isNaN(now) && day >= 0 && day < daysName.length) {
-//     alert(`The day of the week for ${userInput} is ${days}.`);
-// } else {
-//     alert("Invalid date format or date.");
-// }
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
 
-// ===> Question : 4 <===
-function checkCountdown() {
-  let event = document.getElementById("eventDate");
-  let now = new Date();
-  let eventDate = new Date(event.value);
-  let difference = eventDate.getTime() - now.getTime();
-  let difToDay = Math.round(difference / 1000 / 60 / 60 / 24);
-  let countdown = document.getElementById("countdown");
-  countdown.innerText = difToDay;
-}
+const registerForm = document.getElementById('register-form')
+const blogForm = document.getElementById('blog-form')
+const loginForm = document.getElementById('login-form')
+const logoutBtn = document.getElementById('logout')
+const authContainer = document.getElementById('authContainer')
+const blogContainer = document.getElementById('blogContainer')
+let uid = ''
 
-// ===> Question : 5 <===
-function isWeekend() {
-  let inputDate = document.getElementById("inputDate");
-  let dateStr = inputDate.value;
-  const date = new Date(dateStr);
-  const dayOfWeek = date.getDay();
-  return dayOfWeek === 0 || dayOfWeek === 6;
-}
-function dateChecker() {
-  let changeTag = document.getElementById("changeValue");
-  if (isWeekend()) {
-    changeTag.innerHTML = `date falls on a weekend (Saturday or Sunday).`;
+onAuthStateChanged(auth, user => {
+  if (user) {
+    uid = user.uid
+    authContainer.style.display = 'none'
+    blogContainer.style.display = 'block'
+
   } else {
-    changeTag.innerHTML = `date does not fall on a weekend.`;
+    console.log('User is logged out')
+    authContainer.style.display = 'block'
+    blogContainer.style.display = 'none'
   }
-}
+})
 
-// ===> Question : 6 <===
-function futureTimeChecker() {
-  let userInputDate = document.getElementById("userInputDate");
-  let futureDate = new Date(userInputDate.value);
-  let currentDate = new Date();
-  let future = futureDate - currentDate;
-  let hours = Math.floor(future / (1000 * 60 * 60));
-  let minutes = Math.floor((future % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((future % (1000 * 60)) / 1000);
+logoutBtn.addEventListener('click', () => {
+  signOut(auth).then(() => alert('User is logged out.'))
+})
 
-  let futureTime = `${hours} hours ${minutes} minutes ${seconds} seconds`;
-  console.log(futureTime);
-  let showFutureDate = document.getElementById("result");
-  showFutureDate.innerHTML = futureTime;
-}
-
-// ===> Question : 7 <===
-function seasonChecker() {
-  let season = document.getElementById("seasonDate");
-  let seasonDate = new Date(season.value);
-  let monthsArray = [
-    "Jan",
-    "Feb",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let month = seasonDate.getMonth();
-  console.log(monthsArray[month]);
-
-  let displaySeason = document.getElementById("displaySeason");
-
-  if (month === 0 || month === 1 || month === 11) {
-    displaySeason.innerHTML = `${monthsArray[month]} is Winter season`;
+registerForm.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log(e)
+  const userInfo = {
+    fullname: e.target[0].value,
+    email: e.target[1].value,
+    password: e.target[2].value
   }
+  createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+    .then(userCredential => {
+      const user = userCredential.user
+      console.log('user->', user)
+    })
+    .catch(error => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      console.log('errorMessage->', errorMessage)
+    })
+})
 
-  if (month === 2 || month === 3 || month === 4) {
-    displaySeason.innerHTML = `${monthsArray[month]} is Spring season`;
+loginForm.addEventListener('submit', e => {
+  e.preventDefault()
+  console.log(e)
+  const userInfo = {
+    email: e.target[0].value,
+    password: e.target[1].value
   }
+  signInWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+    .then(userCredential => {
+      // Signed up
+      const user = userCredential.user
+      console.log('user logged in->', user)
+      // ...
+    })
+    .catch(error => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      console.log('errorMessage user not logged in->', errorMessage)
 
-  if (month === 5 || month === 6 || month === 7 || month === 8) {
-    displaySeason.innerHTML = `${monthsArray[month]} is Summer Rainy season`;
+      // ..
+    })
+})
+
+blogForm.addEventListener('submit', async e => {
+  e.preventDefault()
+  const blogInfo = {
+    title: e.target[0].value,
+    description: e.target[1].value,
+    level: e.target[2].value,
+    user: uid
   }
-
-  if (month === 9 || month === 10) {
-    displaySeason.innerHTML = `${monthsArray[month]} is Fall season`;
+  try {
+    const docRef = await addDoc(collection(db, 'blogs'), blogInfo)
+    console.log('Document written with ID: ', docRef.id)
+    alert('Document added')
+  } catch (e) {
+    console.error('Error adding document: ', e)
   }
-}
-
-// ===> Question : 8 <===
-function dobCheck() {
-  let date = document.getElementById("birthDate");
-  let userDob = new Date(date.value);
-
-  let futureDate = document.getElementById("futureDate");
-  let userFuture = new Date(futureDate.value);
-
-  let difference = userFuture.getFullYear() - userDob.getFullYear();
-  console.log(difference);
-  let countdown = document.getElementById("futureAge");
-  countdown.innerHTML = `You will be ${difference} years old on ${userFuture.getFullYear()}  `;
-}
-
-// ===> Question : 9 <===
-function daysChecker() {
-  let date1 = document.getElementById("date1");
-  let firstDate = new Date(date1.value);
-  
-  let date2 = document.getElementById("date2");
-  let secDate = new Date(date2.value);
-  let difference = secDate.getTime() - firstDate.getTime() ;
-  let difToDay = Math.round(difference / 1000 / 60 / 60 / 24);
-  let countdown = document.getElementById("calculatedDays");
-  countdown.innerText =`${difToDay} days is Remaining`;
-}
+})
